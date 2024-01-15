9426144dfd5bddd96c54203029bbf9dab67757ef
olomg48 <olomg48@gmail.com>
2024-01-15 22:07:43 +0100

    Wszystkie stopy zwrotu done

--- projekt.py
+++ projekt.py
@@ -1,5 +1,9 @@
+from cgi import test
 import pandas as pd
 import math, numpy as np
+import scipy
+import statsmodels
+from statsmodels import stats
 pko = pd.read_csv('pko_d.csv')
 mil = pd.read_csv('mil_d.csv')
 
@@ -15,10 +19,40 @@ pko['Stopa otwarcie-zamkniecie'] = np.log(pko['Zamkniecie']) - np.log(pko['Otwar
 #stopa zwrotu od zamkniecia do otwarcia
 pko['Stopa zamkniecie-otwarcie'] = np.log(pko['Otwarcie'].shift(1)) - np.log(pko['Zamkniecie'])
 
-#tygodniowa stopa zwrotu
-pko['Tygodniowa stopa zwrotu'] = np.log(pko["Otwarcie"]) - np.log(pko['Otwarcie'].loc[(pko['Data']==pko['Data']-7)])
+# Tutaj przeprowadzane jest mapowanie, które umożliwi dodanie kolumny z zamknięciem sprzed tygodnia
+data_map = {}
+for i in range(len(pko)):
+    current_date = pko.iloc[i]['Data']
+    week_ago = current_date - pd.DateOffset(days=7)
+    # Sprawdzenie, czy data sprzed tygodnia istnieje w danych
+    data_map[current_date] = week_ago if week_ago in pko['Data'].values else pd.NaT
 
-#Przesunięcie spowodowało wartość Nan w pierwszym wierszu - wypełniamy zerem
-pko.fillna(0, inplace=True)
+# Utworzenie nowej kolumny z wartością zamknięcia sprzed tygodnia lub NaN
+pko['Zamkniecie tydzien temu'] = pko['Data'].map(data_map).map(pko.set_index('Data')['Zamkniecie'])
 
-print(pko[['Data', 'Otwarcie','Tygodniowa stopa zwrotu', 'Zamkniecie','Stopa zwrotu', 'Stopa otwarcie-zamkniecie', 'Stopa zamkniecie-otwarcie']].head(30))
+pko['Tygodniowa stopa zwrotu'] = np.log(pko['Zamkniecie']) - np.log(pko['Zamkniecie tydzien temu'])
+
+# Wypełniamy wszystkie NaN zerami // nie wiem w sumie czy wypełniać, czy nie
+#pko.fillna(0, inplace=True)
+
+#Test shapiro
+shapiro1 = scipy.stats.shapiro(pko['Stopa zwrotu'])
+shapiro2 = scipy.stats.shapiro(pko['Tygodniowa stopa zwrotu'])
+
+#Test kolmogorova
+kolmogorov1 = scipy.stats.kstest(pko['Stopa zwrotu'], scipy.stats.norm.cdf)
+kolmogorov2 = scipy.stats.kstest(pko['Tygodniowa stopa zwrotu'], scipy.stats.norm.cdf)
+
+
+#Test chi2
+chi2_1 = scipy.stats.chisquare(pko['Stopa zwrotu'])
+chi2_2 = scipy.stats.chisquare(pko['Tygodniowa stopa zwrotu'])
+
+print(chi2_2)
+
+
+
+
+
+
+#print(pko[['Data', 'Otwarcie','Tygodniowa stopa zwrotu', 'Zamkniecie','Stopa zwrotu', 'Stopa otwarcie-zamkniecie', 'Stopa zamkniecie-otwarcie']].head(30))
